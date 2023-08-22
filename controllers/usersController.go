f3670ec417e4cfe597984f833b4c11b547ba29d1
Saeed Aldosary <SAldosary@Scai.sa>
2023-08-22 08:43:35 +0300

    Create signup feature

---
+++ controllers/usersController.go
@@ -0,0 +1,50 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/saeedmaldosary/Go-Authentication-JWT/initializers"
+	"github.com/saeedmaldosary/Go-Authentication-JWT/models"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Signup(c *gin.Context) {
+	// Get the email/pass off req body
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+
+	// Hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to hash the password",
+		})
+		return
+	}
+
+	// Create the user
+	user := models.User{Email: body.Email, Password: string(hash)}
+
+	result := initializers.DB.Create(&user) // pass pointer of data to Create
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create user",
+		})
+		return
+	}
+
+	// Respond
+	c.JSON(http.StatusOK, gin.H{})
+
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/saeedmaldosary/Go-Authentication-JWT/controllers"
 	"github.com/saeedmaldosary/Go-Authentication-JWT/initializers"
 )
 
@@ -13,10 +14,6 @@ func init() {
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.POST("/signup", controllers.Signup)
 	r.Run()
 }
